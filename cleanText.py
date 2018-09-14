ef22a8e5464e98ead605f858317956c1d5c648ea
AGiantSquid <ashley@greenkeytech.com>
2018-09-14 16:02:37 -0500

    fix file paths, add in launch from binaries option

--- cleanText.py
+++
@@ -1,5 +0,0 @@
-def text2int(wordList, spacer):
-  return spacer.join(wordList)
-  
-def clean_fractions(wordList, spacer):
-  return spacer.join(wordList)
---
+++ launch_discovery.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+'''This module will launch Discovery on http://localhost:1234
+
+This file will automatically scan for a folder of discovery binaries.
+If the binaries are not detected, the script will launch the Discovery docker container.
+
+Available functions:
+- launch_discovery: Starts discovery container or binaries
+'''
+import fnmatch
+import os
+import subprocess
+from discovery_config import DISCOVERY_CONFIG, DISCOVERY_PORT, DISCOVERY_IMAGE_NAME
+
+
+def launch_discovery(custom_directory=None, type=None, port=None, discovery_config=None):
+    '''Launches the Discovery engine either via docker container or via compiled binaries.
+
+    Args:
+        custom_directory: File path to the custom directory would like uploaded to discovery.
+        type: String, can either be 'docker' or 'binaries'
+    '''
+    if custom_directory is None:
+        custom_directory = os.getcwd()
+    if port is None:
+        port = DISCOVERY_PORT
+    if discovery_config is None:
+        discovery_config = DISCOVERY_CONFIG
+    if type is None:
+        type = _determine_discovery_launch_type()
+
+    if type == 'docker':
+        return _launch_container(custom_directory, port, discovery_config)
+
+    return _launch_binaries(custom_directory, port, discovery_config)
+
+
+def _determine_discovery_launch_type():
+    binaries_directory = _detect_binaries_file()
+    if binaries_directory:
+        return 'binaries'
+    return 'docker'
+
+
+def _launch_container(custom_directory, port, discovery_config):
+    '''launches the Discovery docker container.'''
+    dico_dir = ["-v", "{}/dico:/dico".format(custom_directory)] if os.path.isdir(custom_directory + '/dico') else []
+    # yapf: disable
+    launch_command = ' '.join(
+        ["docker", "run", "--rm", "-d"] +
+        ["--name", "discovery-dev"] +
+        ["-v", "{}:/custom".format(custom_directory)] +
+        ["-p", "{}:1234".format(port)] +
+        dico_dir +
+        ["-e {}={}".format(k, v) for k, v in discovery_config.iteritems()] +
+        [DISCOVERY_IMAGE_NAME]
+    )
+    # yapf: enable
+    print("Launching Discovery from Container: {}\n".format(launch_command))
+
+    subprocess.call(launch_command, shell=True)
+
+
+def _launch_binaries(custom_directory, port, discovery_config):
+    '''launches Discovery from the compiled binaries.'''
+    binaries_directory = _detect_binaries_file()
+    if binaries_directory:
+        import sys
+        sys.path.append(os.path.abspath(binaries_directory))
+        from run import find_definition_files_and_copy_them_to_appropriate_location
+        find_definition_files_and_copy_them_to_appropriate_location(
+            os.path.join(custom_directory), os.path.abspath(binaries_directory)
+        )
+
+        sys.path.append(os.path.join(binaries_directory, 'discovery'))
+        os.chdir(os.path.join(binaries_directory, 'discovery', 'server'))
+        print("Launching Discovery from Binaries: {}\n".format(binaries_directory))
+
+        sdk_directory = os.path.abspath(os.path.join(binaries_directory, '..'))
+        # remove any instances of the SDK directory from the path to prevent dummy modules from getting loaded in
+        while sdk_directory in sys.path:
+            sys.path.remove(sdk_directory)
+
+        from server.server import main
+        main()
+
+
+def _detect_binaries_file():
+    '''Returns the absolute path of the binaries directory.'''
+    for file in os.listdir('.'):
+        if fnmatch.fnmatch(file, 'discovery_binaries_*') and not file.endswith('.tar.gz') and not file.endswith('.zip'):
+            return os.path.abspath(file)
+    return None
+
+
+if __name__ == '__main__':
+    directions_custom_directory = os.path.join(os.getcwd(), 'examples', 'directions', 'custom')
+    launch_discovery(custom_directory=directions_custom_directory)
