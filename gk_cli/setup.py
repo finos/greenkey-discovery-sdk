d84ed9bd52486cb8c16272e3b319de498480bf8c
AGiantSquid <ashley@greenkeytech.com>
2018-10-10 15:10:55 -0500

    Ashley/fix entity route and token help (#26)
    * fix entity message equality check
    
    * fix token help message
    
    * update cli version

--- gk_cli/setup.py
+++ gk_cli/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gk_cli',
-    version='0.1.0',
+    version='0.1.1',
     packages=['gk_cli'],
     install_requires=['PyInquirer'],
     entry_points={
