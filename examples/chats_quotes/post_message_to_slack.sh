92795ebee86717d47f6b5938dc4476573ecb1bd0
ageojo <ageojo@greenkeytech.com>
2019-04-05 15:46:30 -0500

    wip

--- examples/chats_quotes/post_message_to_slack.sh
+++ examples/chats_quotes/post_message_to_slack.sh
@@ -12,5 +12,5 @@ SLACK_PASSWORD="xoxp-4953937137-344679330118-509400138422-a20f349799113ba21f5c91
 
 curl -X POST -H "Authorization: Bearer ${SLACK_PASSWORD}" \
 	-H 'Content-type: application/json' \
-	--data '{"channel": "'${CHANNEL_ID}'", "text": "This is a test"}' \
+	--data '{"channel": "'${CHANNEL_ID}'", "text": "This is a test", "as_user":"true"}' \
 	https://slack.com/api/chat.postMessage
