bd095ebcc6a9349f3ba53b2187684fa72a2fb949
Matthew Goldey <matthew.goldey@gmail.com>
2019-05-23 14:45:36 -0500

    Update test_discovery.py

--- test_discovery.py
+++ test_discovery.py
@@ -164,8 +164,9 @@ def submit_transcript(transcript):
     response = requests.post("http://{}:{}/process".format(DISCOVERY_HOST, DISCOVERY_PORT), json=data)
     if response.status_code == 200:
       return response.json()
-    logger.error("Request was not successful. Response Status Code: {}".format(r.status_code))
-    return {}
+    else:
+      logger.error("Request was not successful. Response Status Code: {}".format(response.status_code))
+      return {}
 
 
 def is_valid_response(resp):
