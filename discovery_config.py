3005e8060eea15e82b3347f75c08950023512fcb
AGiantSquid <ashley@greenkeytech.com>
2019-07-02 13:46:53 -0500

    remove logging stuff from config

--- discovery_config.py
+++ discovery_config.py
@@ -9,17 +9,13 @@ DISCOVERY_CONFIG = {
     "STRUCTURE_CONFIDENCE_THRESHOLD": "0.8",
     "SORT_ENTITIES_BY_LENGTH": "True",
     "PORT": "1234",  # Port that Discovery will bind to on the Docker daemon, change if port is taken already
-    "FILE_LOG_LEVEL": "verbose",
-    "CONSOLE_LOG_LEVEL": "verbose",
-    "VERBOSE_LOGGING": "true",
-    "DISCOVERY_DOMAINS": "fruits"
 }
 
 CONTAINER_NAME = "discovery-dev"
 DISCOVERY_PORT = "1234"  # This is the port you will POST to, e.g. curl localhost:1234
 DISCOVERY_SHUTDOWN_SECRET = "greenkeytech"
 DISCOVERY_HOST = "localhost"
-DISCOVERY_IMAGE_NAME = "docker.greenkeytech.com/discovery:ashley"
+DISCOVERY_IMAGE_NAME = "docker.greenkeytech.com/discovery"
 
 TIMEOUT = 10
 RETRIES = 5
