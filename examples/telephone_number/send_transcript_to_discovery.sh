0b2e289d9b8f47aa0435aa88d7426452c276604d
Matthew Goldey <matthew.goldey@gmail.com>
2020-05-28 16:55:25 -0400

    update endpoint from deprecated one

--- examples/telephone_number/send_transcript_to_discovery.sh
+++ examples/telephone_number/send_transcript_to_discovery.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -X POST http://localhost:1234/discover  \
+curl -X POST http://localhost:1234/process  \
      -H "Content-Type: application/json" \
      -d '{"transcript": "You can reach me at five five five four three two five nine eight one"}'
