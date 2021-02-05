c336cbb7d70845b4cbaa55076b382e31a0fd48c1
Matthew Goldey <matthew.goldey@gmail.com>
2021-02-05 12:55:03 -0500

    cut the number of times discovery restarts during testing

--- examples/telephone_number/send_transcript_to_discovery.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -X POST http://localhost:1234/process  \
-     -H "Content-Type: application/json" \
-     -d '{"transcript": "You can reach me at five five five four three two five nine eight one"}'
