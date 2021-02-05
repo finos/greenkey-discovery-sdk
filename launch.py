24c243b5cb28fd60dd596e994f157c401e0e2745
Matthew Goldey <matthew.goldey@gmail.com>
2021-02-05 10:34:59 -0500

    add more handrails around init containg launching and clean up stray volumes and containers immediately

--- launch.py
+++ launch.py
@@ -49,9 +49,9 @@ class RetryRequest(Retry):
     BACKOFF_MAX = float(env["TIMEOUT"])
 
 
-retries = RetryRequest(total=int(env["RETRIES"]),
-                       backoff_factor=1.0,
-                       status_forcelist=[500, 502, 503, 504])
+retries = RetryRequest(
+    total=int(env["RETRIES"]), backoff_factor=1.0, status_forcelist=[500, 502, 503, 504]
+)
 request_session = requests.Session()
 request_session.mount("http://", HTTPAdapter(max_retries=retries))
 
@@ -72,7 +72,8 @@ def pull_image(image):
             get_docker_client().images.pull(image)
     except requests.exceptions.HTTPError:
         LOGGER.warning(
-            "Unable to pull latest %s. Using image as found in local registry", image)
+            "Unable to pull latest %s. Using image as found in local registry", image
+        )
 
 
 def load_builtin_discovery_models():
@@ -89,14 +90,15 @@ def load_builtin_discovery_models():
     initdiscovery_project = "docker.greenkeytech.com/greenkey-discovery-sdk-private"
     initdiscovery_image = f"{initdiscovery_project}:{initdiscovery_tag}"
     pull_image(initdiscovery_image)
-    get_docker_client().containers.run(
+    initcontainer = get_docker_client().containers.run(
         initdiscovery_image,
-        auto_remove=True,
-        volumes={builtin_models.name: {
-            "bind": "/models",
-            "mode": "rw"
-        }},
+        auto_remove=False,
+        detach=True,
+        volumes={builtin_models.name: {"bind": "/models", "mode": "rw"}},
     )
+    initcontainer.start()
+    initcontainer.wait()
+    initcontainer.remove(force=True)
 
 
 def load_builtin_nlprocessor_models():
@@ -113,14 +115,15 @@ def load_builtin_nlprocessor_models():
     initnlprocessor_project = "docker.greenkeytech.com/nlpmodelcontroller"
     initnlprocessor_image = f"{initnlprocessor_project}:{initnlprocessor_tag}"
     pull_image(initnlprocessor_image)
-    get_docker_client().containers.run(
+    initcontainer = get_docker_client().containers.run(
         initnlprocessor_image,
-        auto_remove=True,
-        volumes={builtin_models.name: {
-            "bind": "/models/transformers",
-            "mode": "rw"
-        }},
+        auto_remove=False,
+        detach=True,
+        volumes={builtin_models.name: {"bind": "/models/transformers", "mode": "rw"}},
     )
+    initcontainer.start()
+    initcontainer.wait()
+    initcontainer.remove(force=True)
 
 
 def load_custom_model(interpreter_directory):
@@ -140,12 +143,9 @@ def load_custom_model(interpreter_directory):
     busybox = get_docker_client().containers.run(
         busybox_image,
         "sleep infinity",
-        auto_remove=True,
+        auto_remove=False,
         detach=True,
-        volumes={vol.name: {
-            "bind": destination,
-            "mode": "rw"
-        }},
+        volumes={vol.name: {"bind": destination, "mode": "rw"}},
     )
     file_like_object = io.BytesIO()
     with tarfile.open(fileobj=file_like_object, mode="w") as tar:
@@ -167,8 +167,9 @@ def create_dummy_custom_model():
 
 
 def wait_on_service(address):
-    return request_session.get("/".join([address, "ping"]),
-                               timeout=float(env["TIMEOUT"]))
+    return request_session.get(
+        "/".join([address, "ping"]), timeout=float(env["TIMEOUT"])
+    )
 
 
 def wait_for_everything(target):
@@ -185,8 +186,9 @@ def check_for_license():
     """
     Codify license check
     """
-    assert (env["LICENSE_KEY"] !=
-            ""), "LICENSE_KEY needed. Please set in your environment or client.env"
+    assert (
+        env["LICENSE_KEY"] != ""
+    ), "LICENSE_KEY needed. Please set in your environment or client.env"
 
 
 def create_volumes(target, interpreter_directory):
@@ -217,13 +219,28 @@ def stand_up_compose(target):
         args += ["-f", "discovery.yaml"]
     if target in [LaunchTarget.nlprocessor, LaunchTarget.everything]:
         nlprocessor_image = (
-            f"docker.greenkeytech.com/nlprocessor:{env['NLPROCESSOR_TAG']}")
+            f"docker.greenkeytech.com/nlprocessor:{env['NLPROCESSOR_TAG']}"
+        )
         pull_image(nlprocessor_image)
         args += ["-f", "nlprocessor.yaml"]
 
     args += ["up", "-d", "--force-recreate"]
     with patch.object(sys, "argv", args):
         docker_compose()
+
+
+def prune_volumes():
+    """
+    Prune any volumes not in use
+    """
+    get_docker_client().volumes.prune()
+
+
+def prune_containers():
+    """
+    Prune any containers which have exited
+    """
+    get_docker_client().containers.prune()
 
 
 def launch_docker_compose(target="everything", interpreter_directory=None):
@@ -234,6 +251,8 @@ def launch_docker_compose(target="everything", interpreter_directory=None):
 
     target = LaunchTarget(target)
 
+    prune_containers()
+    prune_volumes()
     create_volumes(target, interpreter_directory)
 
     stand_up_compose(target)
@@ -249,7 +268,9 @@ def remove_volume(volume_name):
     try:
         vol = get_docker_client().volumes.get(volume_name)
         for container in get_docker_client().containers.list():
-            if any(vol.name == mount.get("Name") for mount in container.attrs["Mounts"]):
+            if any(
+                vol.name == mount.get("Name") for mount in container.attrs["Mounts"]
+            ):
                 container.remove(force=True)
         vol.remove()
     except docker.errors.NotFound:
@@ -274,9 +295,9 @@ def teardown_docker_compose():
         docker_compose()
 
     for volume_name in [
-            CUSTOM_DISCOVERY_INTERPRETER,
-            BUILTIN_DISCOVERY_MODELS,
-            BUILTIN_NLPROCESSOR_MODELS,
+        CUSTOM_DISCOVERY_INTERPRETER,
+        BUILTIN_DISCOVERY_MODELS,
+        BUILTIN_NLPROCESSOR_MODELS,
     ]:
         remove_volume(volume_name)
 
