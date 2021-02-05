40916d7e83804d0dc77f3805680c3dd6b2db680e
Matthew Goldey <matthew.goldey@gmail.com>
2021-02-05 13:13:22 -0500

    ensure directories are checked for inconsistent definitions of entities

--- testing/test_directory.py
+++ testing/test_directory.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+import sys
+
+# import pytest to ensure that pytest parametrizes this test with any provided interpreter directory
+import pytest
 
 from validate_yaml import validate_all_files
 
@@ -8,3 +12,6 @@ def test_directory(interpreter_directory):
     Test a directory
     """
     validate_all_files(interpreter_directory)
+
+if __name__ == "__main__":
+    sys.exit(pytest.main(sys.argv))
